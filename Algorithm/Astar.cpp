ab76259b13c2bbfbee6595b667c6d9aaa63d5408
Tim-eyes <18228057503@163.com>
2021-05-06 19:29:26 +0800

    Astar-fixed

--- Algorithm/Astar.cpp
+++ Algorithm/Astar.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 vector<Shape> ans1;
 //vector<vector<Shape>> v;
-int vis[8][10];
+int vis1[8][10];
 int h_x(){
     return 0;
 }
@@ -14,54 +14,55 @@ void Astar(int kase,int depth,int maxed){
     }
     if(depth==7){
         for(auto &it:ans1){
-            cout<<it;
+            cout<<it<<' ';
         }
         //v.emplace_back(ans1);
         cout<<"\n";
+        return;
     }
     for(int i=0;i<8;i++)
         for(int j=0;j<9;j++){
             switch (depth) {
                 case 0:
-                    initial_area(kase);
+                    //initial_area(kase);
                     if (matrix[kase][i][j] == 2) {
                         Square s(Point(i, j));
                         if (check(kase, s)) {
-                            vis[i][j] = depth+1;
+                            vis1[i][j] = depth+1;
                             ans1.emplace_back(s);
-                            dfs(kase, depth + 1);
+                            Astar(kase, depth + 1,0);
                             ans1.pop_back();
-                            vis[i][j] = 0;
+                            vis1[i][j] = 0;
                         }
                     }
                     break;
                 case 1:
-                    if (matrix[kase][i][j] == 2 && !vis[i][j]) {
+                    if (matrix[kase][i][j] == 2 && !vis1[i][j]) {
                         for (int k = 0; k < 4; k++) {
                             BigTriangle bt(Point(i, j), k);
                             if (check(kase, bt) && not_overlap(bt,ans1)) {
-                                vis[i][j] = depth+1;
+                                vis1[i][j] = depth+1;
                                 ans1.emplace_back(bt);
                                 //cout<<bt<<'\n';
-                                dfs(kase, depth + 1);
+                                Astar(kase, depth + 1,0);
                                 ans1.pop_back();
-                                vis[i][j] = 0;
+                                vis1[i][j] = 0;
                             }
 
                         }
                     }
                     break;
                 case 2:
-                    if (matrix[kase][i][j] == 2 && !vis[i][j]) {
+                    if (matrix[kase][i][j] == 2 && !vis1[i][j]) {
                         for (int k = 0; k < 4; k++) {
                             BigTriangle bt(Point(i, j), k);
                             if (check(kase, bt) && not_overlap(bt,ans1)) {
-                                vis[i][j] = depth+1;
+                                vis1[i][j] = depth+1;
                                 ans1.emplace_back(bt);
                                 //cout<<bt<<'\n';
-                                dfs(kase, depth + 1);
+                                Astar(kase, depth + 1,0);
                                 ans1.pop_back();
-                                vis[i][j] = 0;
+                                vis1[i][j] = 0;
                             }
 
                         }
@@ -72,11 +73,11 @@ void Astar(int kase,int depth,int maxed){
                         for(int k=0;k<4;k++){
                             Parallelogram p(Point(i,j),k);
                             if(check(kase,p) && not_overlap(p,ans1)){
-                                vis[i][j]=depth+1;
+                                vis1[i][j]=depth+1;
                                 ans1.emplace_back(p);
-                                dfs(kase,depth+1);
+                                Astar(kase,depth+1,0);
                                 ans1.pop_back();
-                                vis[i][j]=0;
+                                vis1[i][j]=0;
                             }
                         }
                     }
@@ -86,11 +87,11 @@ void Astar(int kase,int depth,int maxed){
                         for(int k=0;k<4;k++){
                             MidTriangle mt(Point(i,j),k);
                             if(check(kase,mt) && not_overlap(mt,ans1)){
-                                vis[i][j]=depth+1;
+                                vis1[i][j]=depth+1;
                                 ans1.emplace_back(mt);
-                                dfs(kase,depth+1);
+                                Astar(kase,depth+1,0);
                                 ans1.pop_back();
-                                vis[i][j]=0;
+                                vis1[i][j]=0;
                             }
                         }
                     }
@@ -100,11 +101,11 @@ void Astar(int kase,int depth,int maxed){
                         for(int k=0;k<4;k++){
                             SmallTriangle st(Point(i,j),k);
                             if(check(kase,st) && not_overlap(st,ans1)){
-                                vis[i][j]=depth+1;
+                                vis1[i][j]=depth+1;
                                 ans1.emplace_back(st);
-                                dfs(kase,depth+1);
+                                Astar(kase,depth+1,0);
                                 ans1.pop_back();
-                                vis[i][j]=0;
+                                vis1[i][j]=0;
                             }
                         }
                     }
